0fe421842cb5b5af6cd6c5a768d3b9850b8a65c4
Bobby Lee <rdlee8181@gmail.com>
2020-08-29 17:07:22 -0700

    Added Ingredients ul to recipe block

--- src/blocks/recipe/recipe.js
+++ src/blocks/recipe/recipe.js
@@ -14,7 +14,7 @@ import recipeIcons from './recipeIcons';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-const { RichText, InnerBlocks } = wp.editor;
+const { RichText, InnerBlocks, BlockList } = wp.editor;
 // console.log(wp.editor);
 
 /**
@@ -30,17 +30,34 @@ const { RichText, InnerBlocks } = wp.editor;
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'sapphire-blocks/recipe', {
+registerBlockType('sapphire-blocks/recipe', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'Sapphire Recipe' ), // Block title.
+	title: __('Sapphire Recipe'), // Block title.
 	icon: recipeIcons.soupSpoon, // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'sapphire-blocks', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [
-		__( 'Sapphire Blocks' ),
-		__( 'Recipe' ),
-		__( 'Cooking' ),
-		__( 'Food' ),
-	],
+	keywords: [__('Sapphire Blocks'), __('Recipe'), __('Cooking'), __('Food')],
+	attributes: {
+		servingSize: {
+			type: 'string',
+			source: 'html',
+			selector: '.serving-size',
+		},
+		prepTime: {
+			type: 'string',
+			source: 'html',
+			selector: '.prep-time',
+		},
+		cookTime: {
+			type: 'string',
+			source: 'html',
+			selector: '.cook-time',
+		},
+		ingredients: {
+			type: 'string',
+			source: 'html',
+			selector: '.ingredients',
+		},
+	},
 
 	/**
 	 * The edit function describes the structure of your block in the context of the editor.
@@ -53,36 +70,58 @@ registerBlockType( 'sapphire-blocks/recipe', {
 	 * @param {Object} props Props.
 	 * @returns {Mixed} JSX Component.
 	 */
-	edit: ( props ) => {
+	edit: (props) => {
+		const onChangeServingSize = (newServingSize) => {
+			props.setAttributes({ servingSize: newServingSize });
+		};
+
+		const onChangePrepTime = (newPrepTime) => {
+			props.setAttributes({ prepTime: newPrepTime });
+		};
+
+		const onChangeCookTime = (newCookTime) => {
+			props.setAttributes({ cookTime: newCookTime });
+		};
+
+		const onChangeIngredients = (newIngredient) => {
+			props.setAttributes({ ingredients: newIngredient });
+		};
+
 		return (
-			<div className={ props.className }>
+			<div className={props.className}>
 				<div className="recipe-meta">
-					<div className="serving-size">
-						<div className="recipe-icon">{ recipeIcons.soupSpoon }</div>
-						<div>
+					<div className="serving-size-wrap">
+						<div className="recipe-icon">{recipeIcons.soupSpoon}</div>
+						<div className="serving-size">
 							<RichText
 								placeholder="Serving Size"
+								value={props.attributes.servingSize}
+								onChange={onChangeServingSize}
 							/>
 						</div>
 					</div>
-					<div className="prep-time">
-						<div className="recipe-icon">{ recipeIcons.apron }</div>
-						<div>
-							<RichText
-								placeholder="Prep Time"
-							/>
+					<div className="prep-time-wrap">
+						<div className="recipe-icon">{recipeIcons.apron}</div>
+						<div className="prep-time">
+							<RichText placeholder="Prep Time" value={props.attributes.prepTime} onChange={onChangePrepTime} />
 						</div>
 					</div>
-					<div className="cook-time">
-						<div className="recipe-icon">{ recipeIcons.pan }</div>
-						<div>
-							<RichText
-								placeholder="Cook Time"
-							/>
+					<div className="cook-time-wrap">
+						<div className="recipe-icon">{recipeIcons.pan}</div>
+						<div className="cook-time-wrap">
+							<RichText placeholder="Cook Time" value={props.attributes.cookTime} onChange={onChangeCookTime} />
 						</div>
 					</div>
 				</div>
-				<InnerBlocks placeholder="Ingredents" allowedBlocks={ [ 'core/list' ] } />
+				{/* <BlockList /> */}
+				{/* <InnerBlocks placeholder="Ingredients" allowedBlocks={ [ 'core/list' ] } /> */}
+				<RichText
+					tagName="ul"
+					multiline="li"
+					placeholder={__('Ingredients', 'sapphire-blocks')}
+					onChange={onChangeIngredients}
+					value={props.attributes.ingredients}
+				/>
 			</div>
 		);
 	},
@@ -101,18 +140,30 @@ registerBlockType( 'sapphire-blocks/recipe', {
 	save: (props) => {
 		return (
 			<div className={props.className}>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>sapphire-blocks</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{' '}
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				<div className="recipe-meta">
+					<div className="serving-size-wrap">
+						<div className="recipe-icon">{recipeIcons.soupSpoon}</div>
+						<div className="serving-size">
+							<RichText.Content value={props.attributes.servingSize} />
+						</div>
+					</div>
+					<div className="prep-time-wrap">
+						<div className="recipe-icon">{recipeIcons.apron}</div>
+						<div className="prep-time">
+							<RichText.Content value={props.attributes.prepTime} />
+						</div>
+					</div>
+					<div className="cook-time-wrap">
+						<div className="recipe-icon">{recipeIcons.pan}</div>
+						<div className="cook-time">
+							<RichText.Content value={props.attributes.cookTime} />
+						</div>
+					</div>
+					<div className="ingredients">
+						<RichText.Content value={props.attributes.ingredients} />
+					</div>
+				</div>
+				{/* <InnerBlocks placeholder="Ingredients" allowedBlocks={ [ 'core/list' ] } /> */}
 			</div>
 		);
 	},
